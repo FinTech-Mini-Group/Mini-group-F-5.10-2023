5ea8412d9f105e1b0e2b0770d81fa52af2f445a2
Faxriddin <akromovfaxriddin1988@gmail.com>
2023-11-14 16:57:58 +0500

    birichi chast

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -1,13 +1,28 @@
 import React from 'react'
+import { icons } from '../utilits/icons'
+import bannerimage from '../assets/banner image.png'
 
 
 function News() {
   return (
-    <div className='container my-[50px]'>
-        <div className=''>
+    <div className='container my-[50px] w-full'>
+        <div className='w-full'>
             <h1 className='text-Cback text-subtitle mb-[30px] '>Будьте в курсе происходящего</h1>
-            <div className=' w-[1282px] bg-banner'>
-                 <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium temporibus quia officia aspernatur a maiores ut cum nobis iste repellat explicabo impedit ipsum unde vel modi necessitatibus eligendi, debitis quod excepturi, doloribus velit atque! Eveniet hic quisquam, laborum velit obcaecati nihil dolor nesciunt consequuntur similique incidunt. Tempora perspiciatis quis quod.</h1>
+            <div className='flex justify-beetween items-center w-full bg-banner bg-no-repeat'>
+                <div className='py-14 px-24'>
+                  <h1 className='text-TitleBold text-Cback mb-[20px]'>Тяжёлое машиностроение — это <br/> вам не песнь светлого будущего</h1>
+                  <p className='text-Body text-Cback mb-[68px]'>Вот вам яркий пример современных тенденций — <br/> экономическая повестка сегодняшнего дня создаёт.</p>
+                  <button className='py-[8px] px-[16px] border-2 border-solid rounded-[4px] bg-bgmain mb-[48px]'>Узнать больше</button>
+                  <div>
+                    <div className='flex items-center gap-10'>
+                      <span className='p-[8px] rounded-[4px] bg-[white]'>{icons.arrowleft}</span>
+                      <span className='p-[8px] rounded-[4px] bg-[white]'>{icons.arrowright}</span>
+                    </div>
+                  </div>
+                </div>
+                <div>
+                  <img src={bannerimage} alt="" />
+                </div>
             </div>
 
         </div>
