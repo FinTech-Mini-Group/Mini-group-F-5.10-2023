779d837cc077b02ab1d0b22abe6934c0c38b4392
faxriddin <akromovfaxriddin@gmail.com>
2023-11-20 09:51:44 +0500

    foter tugirlandi

--- src/landingpages/Footer.jsx
+++ src/landingpages/Footer.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import { icons } from '../utilits/icons'
+import { Partnyor, buyers, company, networks } from '../utilits/footermenu'
+import { Link } from 'react-router-dom'
 
 
 function Footer() {
@@ -7,37 +9,53 @@ function Footer() {
     <div className=' lg:px-[100px] bg-Cwhite lg:pt-[50px] lg:flex justify-between'>
         <div>
             <h1 className='text-subtitle text-subtitles mb-[30px]'>Покупателям</h1>
-            <p className='text-Body text-[#999999] mb-[15px] cursor-pointer '>Как сделать заказ</p>
-            <p className='text-Body text-[#999999] mb-[15px] cursor-pointer '>Способы оплаты</p>
-            <p className='text-Body text-[#999999] mb-[15px] cursor-pointer'>Доставка</p>
-            <p className='text-Body text-[#999999] mb-[15px] cursor-pointer'>Возврат товара</p>
-            <p className='text-Body text-[#999999] mb-[15px] cursor-pointer'>Возврат денежных средств</p>
+           {
+            buyers.map((element, value) => {
+                return <li className='list-none mb-[15px]' key={value}>
+                    <Link className='  text-Body text-[#999999]'>{element.linkName}</Link>
+                </li>
+            })
+           }
         </div>
         <div>
             <h1 className='text-subtitle text-subtitles mb-[30px]'>Партнёрам</h1>
-            <p className='text-Body text-[#999999] mb-[15px] cursor-pointer '>Продать на маркетплейсе</p>
-            <p className='text-Body text-[#999999] mb-[15px] cursor-pointer '>Наши магазины</p>
-            <p className='text-Body text-[#999999] mb-[15px] cursor-pointer'>Перевозчикам</p>
-            <p className='text-Body text-[#999999] mb-[15px] cursor-pointer'>Партнерский пункт выдачи</p>
+           {
+             Partnyor.map((element, value) => {
+                return <li className='list-none mb-[15px]' key={value}>
+                    <Link className='  text-Body text-[#999999]'>{element.linkName}</Link>
+                </li>
+            })
+           }
         </div>
         <div>
             <h1 className='text-subtitle text-subtitles mb-[30px]'>Компания</h1>
-            <p className='text-Body text-[#999999] mb-[15px] cursor-pointer '>О нас</p>
-            <p className='text-Body text-[#999999] mb-[15px] cursor-pointer '>Реквизиты</p>
-            <p className='text-Body text-[#999999] mb-[15px] cursor-pointer'>Пресс-центр</p>
+            {
+             company.map((element, value) => {
+                return <li className='list-none mb-[15px]' key={value}>
+                    <Link className='  text-Body text-[#999999]'>{element.linkName}</Link>
+                </li>
+            })
+           }
+
+            
         </div>
         <div>
             <h1 className='text-subtitle text-subtitles mb-[30px]'>Контакты</h1>
-            <p className='text-Body text-[#999999] mb-[15px] cursor-pointer '>Г. Ташкент, Юнусабадский р.<br/> ул.Юнус Раджаби дом а16</p>
-            <p className='text-Body text-[#999999] mb-[15px] cursor-pointer '>Реквизиты</p>
-            <p className='text-Body text-[#999999] mb-[15px] cursor-pointer'>Пресс-центр</p>
+            <p className='text-Body text-[#999999] mb-[15px] '>Г. Ташкент, Юнусабадский р.<br/> ул.Юнус Раджаби дом а16</p>
+            <p className='text-Body text-[#999999] mb-[15px] '>+998 99 123 45 67</p>
+            <p className='text-Body text-[#999999] mb-[15px] '>help@mail.uz</p>
         </div>
         <div>
             <h1 className='text-subtitle text-subtitles mb-[30px]'>Социальные сети</h1>
-            <div className='flex items-center'>
-                <span className='mr-[30px] p-[10px] border-2 border-solid rounded-[20px] cursor-pointer'>{icons.telegram}</span>
-                <span className='mr-[30px] p-[10px] border-2 border-solid rounded-[20px] cursor-pointer'>{icons.Instagram}</span>
-                <span className='p-[10px] border-2 border-solid rounded-[20px] cursor-pointer'>{icons.Facebook}</span>
+            <div className='flex  items-center'>
+               
+            {
+             networks.map((element, value) => {
+                return <li className='list-none  mr-[30px] p-[10px] border-2 border-solid rounded-[20px]' key={value}>
+                    <Link className=' '>{element.icons}</Link>
+                </li>
+            })
+           }
             </div>
         </div>
     </div>
