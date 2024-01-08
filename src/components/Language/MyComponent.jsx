608d75c328bab5176cae2b68ce76546332c72399
faxriddin <akromovfaxriddin@gmail.com>
2024-01-08 17:09:34 +0500

    yazik qismi ozroq  tugirlandi

--- src/components/Language/MyComponent.jsx
+++ src/components/Language/MyComponent.jsx
@@ -1,19 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useTranslation } from 'react-i18next';
-
+import uzb from '../../assets/uzbekistan-flag-circular-17860.png'
+import rus from '../../assets/russia-flag-circular-17765.png'
+import mir from '../../assets/global-9350.png'
 const MyComponent = () => {
 const { t, i18n } = useTranslation();
 const changeLanguage = (lng) => {
     i18n.
 changeLanguage(lng);
   };
+  const [isOpen, setIsOpen] = useState(false)
   return (
-    <div>
-      <h1>{t('welcome.title')}</h1>
-      <p>{t('welcome.message')}</p>
-      <button onClick={() => changeLanguage('uz')}>O'zbekcha</button>
-      <button onClick={() => changeLanguage('ru')}>Русский</button>
+   <>
+   <div className='relative'>
+   <img onClick={()=>setIsOpen((p)=>!p)} className='w-8' src={mir} alt="" />
+   {(isOpen &&
+    <div className=' absolute top-30 right-[-157px] bg-black p-2 rounded-[8px] pb-6'>
+      {/* <h1 className='text-white'>{t('welcome.title')}</h1>
+      <p className='text-white'>{t('welcome.message')}</p> */}
+      <p onClick={() => changeLanguage('uz')} src={uzb} className='text-white text-Bodystrik flex items-center mb-3 gap-3 cursor-pointer'><img className='w-8'  src={uzb} alt="" />UZBEK</p>
+      <p className='border-2 border-solid mb-3 '></p>
+      <p onClick={() => changeLanguage('ru')} className='text-white text-Bodystrik flex items-center gap-3 cursor-pointer ' ><img className='w-8' onClick={() => changeLanguage('ru')} src={rus} alt="" />RUSSIAN</p>
+       </div>
+       )}
     </div>
+   </> 
   );
 };
 
--- src/components/header/HeaderTop.jsx
+++ src/components/header/HeaderTop.jsx
@@ -1,21 +1,18 @@
+import { t } from 'i18next';
 import React from 'react';
+import { useTranslation } from 'react-i18next';
 import global from '../../assets/icons/global.svg'
 import MyComponent from '../Language/MyComponent';
 
 
-function HeaderTop() {
-
+  
+function HeaderTop(){
   return (
     
     <div className='h-[50px] w-full lg:flex items-center justify-between font-rl'>
         <div className='flex lg:px-20 lg:py-2 gap-7 items-center '>
-            {/* <label htmlFor=""> */}
-                {/* <select name="" id="" className='bg-transparent border-none outline-none' >
-                    <option value=""> Русский </option>
-                </select>
-            </label> */}
             <MyComponent/>
-            <h2 className='text-[#999999]'>Магазины</h2>
+            <h2 className='text-[#999999]'>{t('dokonlar')}</h2>
             <button className='border-2 border-[#00B709] text-xs lg:py-1 lg:px-2 rounded-md text-[#00B709]'>Стать продавцом</button>
         </div>
         <div className=' ml-[150px] mt-[30px] lg:flex lg:gap-7 lg:px-20  '>
