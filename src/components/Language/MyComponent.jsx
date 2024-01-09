36f1b1661bf29d43207c64e2383f673a52633ab9
faxriddin <akromovfaxriddin@gmail.com>
2024-01-09 20:36:32 +0500

    headertop language

--- src/components/Language/MyComponent.jsx
+++ src/components/Language/MyComponent.jsx
@@ -18,7 +18,7 @@ changeLanguage(lng);
     <div className=' absolute top-30 right-[-157px] bg-black p-2 rounded-[8px] pb-6'>
       {/* <h1 className='text-white'>{t('welcome.title')}</h1>
       <p className='text-white'>{t('welcome.message')}</p> */}
-      <p onClick={() => changeLanguage('uz')} src={uzb} className='text-white text-Bodystrik flex items-center mb-3 gap-3 cursor-pointer'><img className='w-8'  src={uzb} alt="" />UZBEK</p>
+      <p onClick={() => changeLanguage('uz')} src={uzb} className='text-white text-Bodystrik flex items-center mb-3 gap-3 cursor-pointer'><img className='w-8' onClick={() => changeLanguage('ru')}  src={uzb} alt="" />UZBEK</p>
       <p className='border-2 border-solid mb-3 '></p>
       <p onClick={() => changeLanguage('ru')} className='text-white text-Bodystrik flex items-center gap-3 cursor-pointer ' ><img className='w-8' onClick={() => changeLanguage('ru')} src={rus} alt="" />RUSSIAN</p>
        </div>
--- src/components/header/HeaderTop.jsx
+++ src/components/header/HeaderTop.jsx
@@ -1,4 +1,4 @@
-import { t } from 'i18next';
+
 import React from 'react';
 import { useTranslation } from 'react-i18next';
 import global from '../../assets/icons/global.svg'
@@ -7,17 +7,18 @@ import MyComponent from '../Language/MyComponent';
 
   
 function HeaderTop(){
+  const { t } = useTranslation();
   return (
     
     <div className='h-[50px] w-full lg:flex items-center justify-between font-rl'>
         <div className='flex lg:px-20 lg:py-2 gap-7 items-center '>
             <MyComponent/>
             <h2 className='text-[#999999]'>{t('dokonlar')}</h2>
-            <button className='border-2 border-[#00B709] text-xs lg:py-1 lg:px-2 rounded-md text-[#00B709]'>Стать продавцом</button>
+            <button className='border-2 border-[#00B709] text-xs lg:py-1 lg:px-2 rounded-md text-[#00B709]'>{t('stat')}</button>
         </div>
         <div className=' ml-[150px] mt-[30px] lg:flex lg:gap-7 lg:px-20  '>
           <h3 className='text-[#999999]'>+99 899 999-99-99</h3>
-          <h3 className='text-[#999999]'>Связаться с нами</h3>
+          <h3 className='text-[#999999]'>{t('svyazat')}</h3>
 
         </div>
 
