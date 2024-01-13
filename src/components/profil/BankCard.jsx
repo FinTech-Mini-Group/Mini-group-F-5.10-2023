840b95f41b7d4fba10cbda741da04a61416d6c06
Faxriddin1988 <148187533+Faxriddin1988@users.noreply.github.com>
2024-01-13 16:41:20 +0300

    Bank kartasi yangilandi

--- src/components/profil/BankCard.jsx
+++ src/components/profil/BankCard.jsx
@@ -13,27 +13,27 @@ function BankCard({visible , onClose}) {
     setCardNumber(formattedInput);
   };
 
-    // Oy va yil ma'lumotlarini saqlash uchun useState
-    const [month, setMonth] = useState('');
-    const [year, setYear] = useState('');
+    // // Oy va yil ma'lumotlarini saqlash uchun useState
+    // const [month, setMonth] = useState('');
+    // const [year, setYear] = useState('');
   
-    // Input o'zgarishlari bo'yicha holatni saqlash
-    const handleMonthChange = (e) => {
-      setMonth(e.target.value);
-       const sanitizedInput = inputText.replace(/\D/g, '').slice(0, 2);
+    // // Input o'zgarishlari bo'yicha holatni saqlash
+    // const handleMonthChange = (e) => {
+    //   setMonth(e.target.value);
+    //    const sanitizedInput = inputText.replace(/\D/g, '').slice(0, 2);
        
-    };
+    // };
   
-    const handleYearChange = (e) => {
-      setYear(e.target.value);
-    };
+    // const handleYearChange = (e) => {
+    //   setYear(e.target.value);
+    // };
   
-    // Forma o'zgarishlarga bo'yicha ishlov berish
-    const handleSubmit = (e) => {
-      e.preventDefault();
-      // Bu yerdagi ma'lumotlarni kerakli ishlov uchun yozishingiz mumkin
-      console.log('Oy:', month, 'Yil:', year);
-    };
+    // // Forma o'zgarishlarga bo'yicha ishlov berish
+    // const handleSubmit = (e) => {
+    //   e.preventDefault();
+    //   // Bu yerdagi ma'lumotlarni kerakli ishlov uchun yozishingiz mumkin
+    //   console.log('Oy:', month, 'Yil:', year);
+    // };
 
 
   return (
@@ -41,14 +41,36 @@ function BankCard({visible , onClose}) {
         <div className='bg-[#FFFFFF] p-[30px] rounded-[10px]'>
                 <h1 className='text-TitleBold mb-5 '>Привязка карты</h1>
                 <div className='w-[480px] h-[380px] px-4 flex gap-8 items-start justify-center flex-col shadow-[0px_0px_20px_1px_rgba(0,0,0,0.10)] '>
-                  <input onChange={handleChange}   maxLength={19} className='bg-[#EFEFF4] p-3 w-full flex justify-center' type="text" id="cardNumber" name="cardNumber" value={cardNumber} placeholder='5 5 6 7    7 8 4 4    3 3 4 4    6 7 8 9' />
+                  <input onChange={handleChange}   maxLength={19} className='bg-[#EFEFF4] p-3 w-full flex justify-center' type="text" id="cardNumber" name="cardNumber" value={cardNumber} placeholder='XXXX XXXX XXXX XXXX' />
                   <div className='flex items-center'>
-                    <form className='flex items-center' onSubmit={handleSubmit} action="">
+                    <form className='flex items-center' action="">
                       <label htmlFor="">
-                  <input onChange={handleMonthChange} value={month} className='py-[22px] pr-[33px] pl-[22px] rounded-[5px] bg-[#EFEFF4] w-[110px]' type="text" placeholder='M M'/>
+                      <select className='py-[22px] pl-[40px] pr-[22px] rounded-[5px] bg-[#EFEFF4] w-[110px]' name="" id="">
+                  <option value="1900">01</option>
+                  <option value="1900">02</option>
+                  <option value="1900">03</option>
+                  <option value="1900">04</option>
+                  <option value="1900">06</option>
+                  <option value="1900">07</option>
+                  <option value="1900">08</option>
+                  <option value="1900">09</option>
+                  <option value="1900">10</option>
+                  <option value="1900">11</option>
+                  <option value="1900">12</option>
+                  </select>
+                  {/* <input  className='py-[22px] pr-[33px] pl-[22px] rounded-[5px] bg-[#EFEFF4] w-[110px]' type="text" placeholder='M M'/> */}
                   </label>
                   <p className='mx-[22px]'>/</p>
-                  <input onChange={handleYearChange} value={year} className='py-[22px] pl-[40px] pr-[22px] rounded-[5px] bg-[#EFEFF4] w-[110px]' type="text" placeholder='Y Y'/>
+                  <select className='py-[22px] pl-[40px] pr-[22px] rounded-[5px] bg-[#EFEFF4] w-[110px]' name="" id="">
+                  <option value="1900">24</option>
+                  <option value="1900">25</option>
+                  <option value="1900">26</option>
+                  <option value="1900">27</option>
+                  <option value="1900">28</option>
+                  <option value="1900">29</option>
+                  <option value="1900">30</option>
+                  </select>
+                  {/* <input onChange={handleYearChange} value={year} className='py-[22px] pl-[40px] pr-[22px] rounded-[5px] bg-[#EFEFF4] w-[110px]' type="text" placeholder='Y Y'/> */}
                   </form>
                   </div>
                 </div>
