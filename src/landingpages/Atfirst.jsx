2011a05cf263bf764c3f0735c3b89222e444b185
faxriddin <akromovfaxriddin@gmail.com>
2024-01-24 10:57:23 +0500

    Category paginatsiya buldi

--- src/landingpages/Atfirst.jsx
+++ src/landingpages/Atfirst.jsx
@@ -8,12 +8,12 @@ function Atfirst() {
         <button onClick={()=>setIsOpen((p)=>!p)} className='text-subtit flex items-center ml-[992px]'>По рейтингу <span className='ml-[10px]'>{icons.arrowb}</span></button>
         {isOpen && (
           <div className='absolute left-[1000px] p-8 bg-white rounded-[5px] w-[310px] border-solid border-2'>
-                <button className='text-subtit mb-8 underline hover:bg-Cmain hover:text-white '>Сначала популярные</button>
-                <button className='text-subtit mb-8 underline hover:bg-Cmain hover:text-white'>Сначала дорогие</button>
-                <button className='text-subtit mb-8 underline hover:bg-Cmain hover:text-white'>Сначала дешёвые</button>
-                <button className='text-subtit mb-8 underline hover:bg-Cmain hover:text-white'>Сначала популярные</button>
-                <button className='text-subtit mb-8 underline hover:bg-Cmain hover:text-white'>Сначала новинки</button>
-                <button className='text-subtit underline hover:bg-Cmain hover:text-white'>По размеру скидки</button>
+                <button className='text-subtit mb-8 cursor-pointer underline hover:bg-Cmain hover:text-white '>Сначала популярные</button>
+                <button className='text-subtit mb-8 cursor-pointer underline hover:bg-Cmain hover:text-white'>Сначала дорогие</button>
+                <button className='text-subtit mb-8 cursor-pointer underline hover:bg-Cmain hover:text-white'>Сначала дешёвые</button>
+                <button className='text-subtit mb-8 cursor-pointer underline hover:bg-Cmain hover:text-white'>Сначала популярные</button>
+                <button className='text-subtit mb-8 cursor-pointer underline hover:bg-Cmain hover:text-white'>Сначала новинки</button>
+                <button className='text-subtit underline cursor-pointer hover:bg-Cmain hover:text-white'>По размеру скидки</button>
                 
 
           </div>
--- src/landingpages/Category.jsx
+++ src/landingpages/Category.jsx
@@ -9,8 +9,18 @@ import { useCategorysQuery } from '../services/categoryApi'
 function Category() {
   const navigate = useNavigate()
   const {data:categories}=useCategorysQuery()
-  console.log(categories)
+  // console.log(categories)
 
+  const [first, setFirst] = useState(1)
+    const cardProduct = 6
+    const lastProductKey = first * cardProduct
+    const firstProductKey = lastProductKey - cardProduct
+
+    const res = categories?.Success?.slice(firstProductKey, lastProductKey)
+    // console.log(res);
+    const numbers = [...Array(Math.floor(categories?.Success?.length / cardProduct) + 2).keys()].slice(1)
+    console.log(numbers)
+ 
   return (
     <div className='lg:container m-auto my-[50px]'>
 
@@ -21,14 +31,21 @@ function Category() {
       <div className='lg:flex justify-between mb-[35px]'>
         <div className='lg:grid lg:grid-cols-6 gap-6'>
           {
-            categories && categories.Success.map((element, value) =>
+            categories && res.map((element, value) =>
               <div className='' key={value}>
                 <img className='w-[262px] h-[262px] rounded-[10px]' src={element.images || Img1} alt="img" />
                 <p className=' mb-[50px] lg:text-center mt-[10px] text-subtitle text-[#000000]'>{element.name}</p>
               </div>
             )}
-        </div>
+        </div>   
       </div>
+               {
+                    <ul className='pagenation flex gap-8 '>
+                        {
+                            numbers?.map((element, value) => <li key={value} className={`${element === first ? 'activePage' : '' } py-2 px-4 border-2 border-solid rounded-[4px] hover:bg-Cmain hover:text-Cwhite `} onClick={() => setFirst(element)}>{element}</li>)
+                        }
+                    </ul>
+                }
     </div>
   )
 }
